186c4f2bc12ef05eea21934502f2aff5f611e0f5
xxxihqt <731992307@qq.com>
2018-11-23 11:12:59 +0800

    finally finish

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -11,5 +11,48 @@ Page({
         return util.formatTime(new Date(log))
       })
     })
+    this.getdata();
+  },
+  getdata(){
+    let getMusicRankings = new Promise((resolve, reject) => {
+      wx.request({
+        url: 'https://api.apiopen.top/musicRankings',
+        header: {
+          'content-type': 'application/json'
+        },
+        success(res) {
+          resolve(res);
+        }
+      })
+    })
+    let getNews = new Promise((resolve, reject) => {
+      wx.request({
+        url: 'https://www.apiopen.top/journalismApi',
+        header: {
+          'content-type': 'application/json'
+        },
+        success(res) {
+          resolve(res);
+        }
+      })
+    })
+    let getDailyVideo = new Promise((resolve, reject) => {
+      wx.request({
+        url: 'https://api.apiopen.top/todayVideo',
+        header: {
+          'content-type': 'application/json'
+        },
+        success(res) {
+          resolve(res);
+        }
+      })
+    })
+    Promise.all([getMusicRankings, getDailyVideo, getNews])
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 })
